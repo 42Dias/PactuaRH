041724f9c141477c72128d8f04bf2dbb8f9c6dcd
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-31 12:24:40 -0300

    used toastContainer to fix toast style

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,27 @@
 // import { SIGN_IN, SIGN_UP, TERMS } from 'routes'
 import RoutesApp from './routes'
 import { GlobalStyle } from 'ui'
+import { ToastContainer } from "react-toastify";
+
 
 function App() {
   return (
     <>
       <RoutesApp />
       <GlobalStyle />
+
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      
     </>
   )
 }
