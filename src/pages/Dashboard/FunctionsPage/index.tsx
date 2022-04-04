5fd08dd2afaee3635933fce7d7a14644fec24d52
Jonntz <monteirojonatas23@gmail.com>
2022-04-04 17:46:23 -0300

    funcao

--- src/pages/Dashboard/FunctionsPage/index.tsx
+++ src/pages/Dashboard/FunctionsPage/index.tsx
@@ -2,12 +2,21 @@ import Sidebar from 'ui/components/Sidebar'
 import Modal from 'react-modal'
 import { FiPlus, FiEye, FiEdit, FiTrash, FiX } from 'react-icons/fi'
 import * as S from './FunctionsPage.styled'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import InputMask from 'react-input-mask'
+import funcoes from 'service/funcoes/funcoes'
 
 export default function FunctionsPage() {
   const [modalIsOpen, setIsOpen] = useState(false)
   const [modalIsOpenNew, setIsOpenNew] = useState(false)
+  const [allFuncoes, setAllFuncoes] = useState([])
+  const [nome, setNome] = useState<string>('')
+  const [funcaoEdit, setFuncaoEdit] = useState<any>({})
+
+  async function getAllFuncoes() {
+    const funcao = await funcoes.list()
+    setAllFuncoes(funcao)
+  }
 
   function openModal() {
     setIsOpen(true)
@@ -24,6 +33,44 @@ export default function FunctionsPage() {
   function closeModalNew() {
     setIsOpenNew(false)
   }
+
+  useEffect(() => {
+    getAllFuncoes()
+  }, [])
+
+  async function createFuncao() {
+    const data = {
+      nome: nome,
+    }
+    console.log(data)
+    console.log(data.nome)
+
+    const isCreated = await funcoes.cadastro(data)
+    console.log(isCreated)
+
+    if (isCreated) closeModalNew()
+    getAllFuncoes()
+  }
+
+  async function updateFuncao(id: string) {
+    const data = {
+      nome: nome,
+    }
+    console.log(data)
+    console.log(data.nome)
+
+    const isUpdated = await funcoes.update(id, data)
+    console.log(isUpdated)
+
+    if (isUpdated) closeModal()
+    getAllFuncoes()
+  }
+
+  async function deleteFuncao(id: string) {
+    const isDelete = await funcoes.delete(id)
+    getAllFuncoes()
+  }
+
   return (
     <>
       <S.Body>
@@ -37,31 +84,40 @@ export default function FunctionsPage() {
               Novo <FiPlus size={18} color='#fff' />
             </button>
           </S.FlexButtons>
+          {allFuncoes.length > 0 && (
+            // eslint-disable-next-line react/jsx-key
+            <S.Table>
+              <S.TrTitle>
+                <td>Função</td>
+              </S.TrTitle>
 
-          <S.Table>
-            <S.TrTitle>
-              <td>Função</td>
-              <td>Mão de obra</td>
-              <td>Grau de instrução</td>
-              <td>Requisito da função</td>
-            </S.TrTitle>
-            <S.TrSecond>
-              <td>Dev</td>
-              <td>front end</td>
-              <td>Ensino medio</td>
-              <td>Desenvolvimento</td>
-              <td>
-                <button onClick={openModal}>
-                  <FiEdit size={18} />
-                </button>
-              </td>
-              <td>
-                <button>
-                  <FiTrash size={18} />
-                </button>
-              </td>
-            </S.TrSecond>
-          </S.Table>
+              {allFuncoes.map((funcao: any, index) => (
+                <S.TrSecond key={index}>
+                  <td>{funcao.nome}</td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        setFuncaoEdit(funcao)
+                        openModal()
+                      }}
+                    >
+                      <FiEdit size={18} />
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        deleteFuncao(funcao.id)
+                      }}
+                    >
+                      <FiTrash size={18} />
+                    </button>
+                  </td>
+                </S.TrSecond>
+              ))}
+            </S.Table>
+          )}
+          {allFuncoes.length === 0 && <p>Não há funções cadastradas!</p>}
         </S.Container>
       </S.Body>
 
@@ -79,18 +135,30 @@ export default function FunctionsPage() {
           <FiX />
         </button>
 
-        <S.ContainerForm>
+        <S.ContainerForm
+          onSubmit={(e: any) => {
+            e.preventDefault()
+            // e.target.reset()
+            updateFuncao(funcaoEdit.id)
+          }}
+        >
           <h2>Editar Função</h2>
 
-          <input type='text' placeholder='Função' />
-          <input type='text' placeholder='Mão de obra' />
+          <input
+            type='text'
+            placeholder='Função'
+            defaultValue={funcaoEdit?.nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+          {/* <input type='text' placeholder='Mão de obra' />
           <select>
             <option>Trabalho em equipe?</option>
             <option>Sim</option>
             <option>Não</option>
           </select>
           <input type='text' placeholder='Grau de instrução' />
-          <input type='text' placeholder='Requisito da função' />
+          <input type='text' placeholder='Requisito da função' /> */}
+          <button type='submit'>Salvar</button>
         </S.ContainerForm>
       </Modal>
 
@@ -108,20 +176,30 @@ export default function FunctionsPage() {
           <FiX />
         </button>
 
-        <S.ContainerForm>
+        <S.ContainerForm
+          onSubmit={(e: any) => {
+            e.preventDefault()
+            // e.target.reset()
+            createFuncao()
+          }}
+        >
           <h2>Cadastrar função</h2>
 
-          <input type='text' placeholder='Função' />
-          <input type='text' placeholder='Mão de obra' />
+          <input
+            type='text'
+            placeholder='Função'
+            onChange={(e) => setNome(e.target.value)}
+          />
+          {/* <input type='text' placeholder='Mão de obra' />
           <select>
             <option>Trabalho em equipe?</option>
             <option>Sim</option>
             <option>Não</option>
           </select>
           <input type='text' placeholder='Grau de instrução' />
-          <input type='text' placeholder='Requisito da função' />
+          <input type='text' placeholder='Requisito da função' /> */}
 
-          <button>Enviar</button>
+          <button type='submit'>Enviar</button>
         </S.ContainerForm>
       </Modal>
     </>
---
+++ src/service/funcoes/funcoes.js
@@ -0,0 +1,143 @@
+import {
+  api,
+  apiWithoutTenant,
+  apiWithoutTenantAndWithToken,
+  apiWithTenantAndWithToken,
+  ip,
+  porta,
+} from '../api'
+
+import responseHandler from '../../utils/responseHandler'
+import servidorErrorMessage from '../../utils/servidorErrorMessage'
+import handleLocalStorageEmailAndPassword from 'utils/handleLocalStorageEmailAndPassword'
+import HandleLocalStorageData from 'utils/handleLocalStorage'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
+export default class funcoes {
+  static async update(id, data) {
+    const response = await api
+      .put(`funcao/${id}`, {
+        data,
+      })
+      .catch(() => {
+        servidorErrorMessage()
+      })
+    const mensagemOk = 'Função alterado com sucesso!'
+    const mensagemNaoOK = 'Revise seus dados :('
+    responseHandler(response.status, mensagemOk, mensagemNaoOK)
+
+    const responseData = response.data
+
+    return responseData
+  }
+
+  static async delete(id) {
+    const response = await api
+      .delete(`funcao/${id}`)
+      .then((res) => {
+        const status = res.status
+        const mensagemOk = 'Modulo apagado com sucesso!'
+        const mensagemNaoOK = 'Algo deu errado :('
+
+        responseHandler(status, mensagemOk, mensagemNaoOK)
+      })
+
+      .catch(() => {
+        servidorErrorMessage()
+      })
+
+    return response
+  }
+
+  static async list() {
+    const response = await api.get('funcao').catch(() => {
+      servidorErrorMessage()
+    })
+
+    const responseData = response.data.rows
+
+    return responseData
+  }
+
+  static async listWithFilter(filter, value) {
+    const response = await api
+      .get(`user?filter%5B${filter}%5D=${value}`)
+      .catch(() => {
+        servidorErrorMessage()
+      })
+
+    const responseData = response.data.rows
+
+    return responseData
+  }
+
+  static async listWithManyFilters(filters) {
+    const response = await api.get(`user?${filters}`).catch(() => {
+      servidorErrorMessage()
+    })
+
+    const responseData = response.data.rows
+
+    return responseData
+  }
+
+  static async find(id) {
+    const response = await api
+      .get(`user/${id}`)
+
+      .catch(() => {
+        servidorErrorMessage()
+      })
+
+    return response.data
+  }
+
+  static async cadastro(data) {
+    return api
+      .post('funcao', {
+        data,
+      })
+      .then(async (response) => {
+        const mensagemOk = 'Opa, recebemos o seu registro :)'
+        console.log(response)
+        responseHandler(response.status, mensagemOk)
+        return response
+      })
+      .catch(() => {
+        servidorErrorMessage()
+      })
+  }
+
+  static async loadUser(token) {
+    const response = await axios({
+      method: 'get',
+      url: `${ip}:${porta}/api/auth/me`,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+      timeout: 50000,
+    }).then((response) => {
+      return response.data
+    })
+
+    const fullName = response.fullName
+    const newRoleLocal = response.tenants[0].roles[0]
+    const newTenatId = response.tenants[0].tenant.id
+    const newId = response.id
+    const newStatus = response.tenants[0].status
+    // let empresaId = response.empresaId
+    HandleLocalStorageData(
+      newRoleLocal,
+      newTenatId,
+      newId,
+      newStatus,
+      token,
+      fullName,
+    )
+
+    return response
+  }
+}
