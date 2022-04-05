ea9d5372f5e8cf4436061723e9aed5786143272f
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-04-05 08:19:42 -0300

    added the functions

--- src/pages/Dashboard/Benefits/index.tsx
+++ src/pages/Dashboard/Benefits/index.tsx
@@ -2,12 +2,16 @@ import Sidebar from 'ui/components/Sidebar'
 import Modal from 'react-modal'
 import { FiPlus, FiEye, FiEdit, FiTrash, FiX } from 'react-icons/fi'
 import * as S from './Benefits.styled'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import InputMask from 'react-input-mask'
+import beneficio from 'service/beneficio/beneficio'
 
 export default function Benefits() {
   const [modalIsOpen, setIsOpen] = useState(false)
   const [modalIsOpenNew, setIsOpenNew] = useState(false)
+  const [nome      , setNome      ] = useState<string>("")
+  const [benefits      , setBenefits      ] = useState<any[]>([])
+
 
   function openModal() {
     setIsOpen(true)
@@ -24,6 +28,36 @@ export default function Benefits() {
   function closeModalNew() {
     setIsOpenNew(false)
   }
+
+  async function handleLoadBenefits() {
+    let allBenefits = await beneficio.list()
+
+    setBenefits(allBenefits)
+  }
+
+  async function handleCreate(){
+    let data = {
+      nome: nome
+    }
+
+    let isCreated = await beneficio.create(data)
+
+    if(isCreated) closeModalNew()
+    await handleLoadBenefits()
+
+  }
+
+
+  useEffect(
+    () => {
+      handleLoadBenefits() 
+    }, []
+  )
+
+
+  async function handleDelete(){
+    
+  }
   return (
     <>
       <S.Body>
@@ -39,13 +73,12 @@ export default function Benefits() {
           </S.FlexButtons>
 
           <S.Table>
+
             <S.TrTitle>
               <td>Nome do benefício</td>
-              <td>Razão Social</td>
-              <td>Data admissão</td>
-              <td>Data demissão</td>
+              <td></td>
             </S.TrTitle>
-            <S.TrSecond>
+              <S.TrSecond>
               <td>Auxilio infraestrutura</td>
               <td>front end</td>
               <td>25/09/2022</td>
@@ -60,8 +93,11 @@ export default function Benefits() {
                   <FiTrash size={18} />
                 </button>
               </td>
-            </S.TrSecond>
-          </S.Table>
+              </S.TrSecond> )
+              )
+              
+
+            </S.Table>
         </S.Container>
       </S.Body>
 
@@ -107,19 +143,26 @@ export default function Benefits() {
           <FiX />
         </button>
 
-        <S.ContainerForm>
+        <S.ContainerForm
+        onSubmit={(e) => {
+          e.preventDefault()
+          handleCreate()
+        }}
+        >
           <h2>Cadastrar benefício</h2>
 
-          <input type='text' placeholder='Nome do benefício' />
-          <input type='text' placeholder='Descrição' />
-          <input type='text' placeholder='Operadora da assistência médica' />
-          <input type='text' placeholder='Razão Social' />
-          <input type='number' placeholder='CNPJ' />
-          <input type='text' placeholder='Registro da ASN' />
-          <input type='number' placeholder='Data admissão ' />
-          <input type='number' placeholder='Data demissão' />
+          <input
+            type='text'
+            onChange={(e) => setNome(e.target.value)}
+            placeholder='Nome do benefício'
+            required
+          />
 
-          <button>Enviar</button>
+          <button
+            type='submit'
+          >
+            Enviar
+          </button>
         </S.ContainerForm>
       </Modal>
     </>
