46e4fcc26e0ce5a22bd7068a51a52f8251e42d65
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-04-07 09:04:17 -0300

    created a basic type with id and name iData

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export interface iDependent  {
+    nome:     string,
+    cpf:      string,
+    rg:       string,
+    dataNasc: string 
+
+}
+
+export interface iData  {
+    id:      string,
+    nome:     string,
+}
