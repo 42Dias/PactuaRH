f48da678b1d106ab26330bf5cfdad83f84c510c0
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-04-12 13:48:33 -0300

    now it goes to prod server

--- src/service/api.js
+++ src/service/api.js
@@ -13,8 +13,8 @@ export const semImagem =
   'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Item_sem_imagem.svg/1024px-Item_sem_imagem.svg.png'
 export const loadingGif = 'https://contribua.org/mb-static/images/loading.gif'
 
-export const ip = 'http://localhost' // teste local
-// export let ip = "https://projetos.42dias.com.br";//servidor/
+// export const ip = 'http://localhost' // teste local
+export let ip = "https://projetos.42dias.com.br";//servidor/
 
 export const porta = '8154' // teste local
 // export let porta = '8154' // servidor
