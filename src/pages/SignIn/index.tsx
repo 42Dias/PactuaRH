b6a886ae320e261184a374cb2415c7bede996762
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-04-13 09:33:17 -0300

    test with new redirect

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -43,7 +43,7 @@ export default function SignIn() {
   }
 
   function checkIfIsLogged(){
-    if(token) window.location.pathname = '/dashboard'
+    if(token) navigate('/dashboard', { })
   }
 
   useLayoutEffect(
