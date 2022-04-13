ef21d86f4e0370fc87a301694da65a2a4e2e4b0d
Jonntz <monteirojonatas23@gmail.com>
2022-04-13 10:12:33 -0300

    costcenter

--- src/routes.tsx
+++ src/routes.tsx
@@ -19,6 +19,7 @@ import Education from 'pages/Dashboard/Education'
 import Skills from 'pages/Dashboard/Skills'
 import Area from 'pages/Dashboard/Area'
 import Box from 'pages/Dashboard/9Box'
+import CostCenter from 'pages/Dashboard/costCenter'
 
 const RoutesApp = () => {
   return (
@@ -34,6 +35,7 @@ const RoutesApp = () => {
         <Route path='/profissionais' element={<Professionals />} />
         <Route path='/cargos' element={<Positions />} />
         <Route path='/funcoes' element={<FunctionsPage />} />
+        <Route path='/centroCustos' element={<CostCenter />} />
         {/*  */}
         <Route path='/area' element={<Area />} />
         <Route path='/beneficios' element={<Benefits />} />
@@ -44,7 +46,7 @@ const RoutesApp = () => {
         <Route path='/planos' element={<Plans />} />
         <Route path='/desempenho' element={<Performance />} />
         <Route path='/avaliacao-geral' element={<GeneralEvaluation />} />
-         <Route path='/relatorio-9box' element={<Box />} />
+        <Route path='/relatorio-9box' element={<Box />} />
       </Routes>
     </BrowserRouter>
   )
