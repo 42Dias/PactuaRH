ade74976b01519a33e9863c24c18d922227e2d52
AlsMartins <alissonmartins589@gmail.com>
2022-04-19 13:57:05 -0300

    atualização api

--- src/service/api.js
+++ src/service/api.js
@@ -16,14 +16,10 @@ export const loadingGif = 'https://contribua.org/mb-static/images/loading.gif'
 export const ip = 'http://localhost' // teste local
 // export let ip = "https://projetos.42dias.com.br";//servidor/
 
-<<<<<<< HEAD
-export const porta = '8154' // teste local
-=======
 // export let ip = "http://127.0.0.1";//teste local
 // export let ip = "https://projetos.42dias.com.br";//servidor/
 
 export let porta = '8154'    // teste local
->>>>>>> 0f0cc714611138ef113d7f95e15f06bbd7c4abe4
 // export let porta = '8154' // servidor
 //***************************************************************************************/
 export const api = axios.create({
