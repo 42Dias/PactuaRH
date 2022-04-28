98700953d15eee1af03bfa78e5439ca56493cb3a
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-04-28 14:59:36 -0300

    created a service to avaliations

---
+++ src/service/avaliacoes/avaliacoes.js
@@ -0,0 +1,120 @@
+
+import {api, apiWithoutTenant, apiWithoutTenantAndWithToken, apiWithTenantAndWithToken, ip, porta} from '../api'
+
+import responseHandler from '../../utils/responseHandler'
+import servidorErrorMessage from '../../utils/servidorErrorMessage'
+import handleLocalStorageEmailAndPassword from 'utils/handleLocalStorageEmailAndPassword'
+import HandleLocalStorageData from 'utils/handleLocalStorage'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
+
+export default class avaliacoes{
+  static async create(data){
+    let response = await api.post(`avaliacao`, {
+      data
+      })
+
+      .catch(() => {
+        servidorErrorMessage()
+      })
+
+      let mensagemOk = 'Benefício criado com sucesso!'
+      let mensagemNaoOK = 'Revise seus dados :('
+      responseHandler(response.status, mensagemOk, mensagemNaoOK)
+
+      let responseData = response.data
+      return responseData
+  }
+//==========================================================================================================
+  static async update(id, data){
+
+    let response = await api.put(`avaliacao/${id}`, {
+      data
+      })
+      .catch(() => {
+        servidorErrorMessage()
+      })
+
+
+      let mensagemOk = 'Benefício alterado com sucesso!'
+      let mensagemNaoOK = 'Revise seus dados :('
+      responseHandler(response.status, mensagemOk, mensagemNaoOK)
+
+      let responseData = response.data
+
+      return responseData    
+
+  }
+//==========================================================================================================
+  static async delete(id){
+
+    let response = await api.delete(`avaliacao/${id}`)
+    .then(
+      (res) => {
+          
+          let status = res.status 
+          let mensagemOk    = 'Modulo apagado com sucesso!'
+          let mensagemNaoOK = 'Algo deu errado :('
+
+          responseHandler(status, mensagemOk, mensagemNaoOK)
+      })
+
+      .catch(() => {
+        servidorErrorMessage()
+      })
+
+      return response
+  }
+//==========================================================================================================
+  static async list(){
+
+    let response = await api.get(`avaliacao`)
+      .catch(() => {
+        servidorErrorMessage()
+      })
+
+      let responseData = response.data.rows
+
+      return responseData
+    
+  }
+
+  static async listWithFilter(filter ,value){
+
+    let response = await api.get(`avaliacao?filter%5B${filter}%5D=${value}`)
+      .catch(() => {
+        servidorErrorMessage()
+      })
+
+      let responseData = response.data.rows
+
+      return responseData
+    
+  }
+//==========================================================================================================
+  static async listWithManyFilters(filters){
+    let response = await api.get(`avaliacao?${filters}`)
+      .catch(() => {
+        servidorErrorMessage()
+      })
+
+      let responseData = response.data.rows
+
+      return responseData
+    
+  }
+//==========================================================================================================
+  static async find(id){
+
+    let response = await api.get(`avaliacao/${id}`)
+
+      .catch(() => {
+        servidorErrorMessage()
+      })
+
+      return response.data
+    
+  }
+
+}
