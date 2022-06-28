071749c0abfdd0d4e3b3b77f2029ebe94a978cb8
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-06-28 09:19:06 -0300

    fixed the CheckBox style

--- src/ui/components/CheckBox/index.tsx
+++ src/ui/components/CheckBox/index.tsx
@@ -1,10 +1,12 @@
 import { PropsCheckBox } from "types";
+import { Container } from "./styles";
 
 export default function CheckBox({checkBoxTitle, checked, onChange}: PropsCheckBox) {
     return (
-      <div>
+      <Container>
         <input checked={checked} type="radio" onChange={(e) => onChange?.(e.target.value)}/>
         <small>{checkBoxTitle}</small>
-      </div>
+      </Container>
     )
   }
+
---
+++ src/ui/components/CheckBox/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+display: flex;
+gap: 0 10px;
+align-items: center;
+
+    input[type='checkbox'] {
+    width: 15px;
+    height: 15px;
+    }
+
+    input[type='radio'] {
+    width: 15px;
+    height: 15px;
+    }
+
+
+`
