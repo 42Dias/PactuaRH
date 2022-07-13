31a1e9e6b8c3e13b7dd10011103f73bdb2e88cbb
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-07-13 10:49:13 -0300

    changed the name of costCenter to CostCenter as good pratices

--- src/pages/Dashboard/CostCenter/index.tsx
+++ src/pages/Dashboard/CostCenter/index.tsx
@@ -9,21 +9,38 @@ import centroCustos from 'service/centroCustos/centroCustos'
 import cargos from 'service/cargos/cargos'
 //@ts-ignore
 import ReactHTMLTableToExcel from 'react-html-table-to-excel'
+import LoadingLayer from 'ui/components/LoadingLayer'
 
 export default function CostCenter() {
-  const [modalIsOpen, setIsOpen] = useState(false)
-  const [modalIsOpenNew, setIsOpenNew] = useState(false)
+
+  //===================================== Modal's States
+  const [modalIsOpen       , setIsOpen     ] = useState(false)
+  const [modalIsOpenNew    , setIsOpenNew  ] = useState(false)
   const [modalIsOpenFilter ,setIsOpenFilter] = useState(false)
-  const [nome, setNome] = useState<string>('')
+
+  //===================================== CRUD's States
+  const [nome     , setNome] = useState<string>('')
   const [descricao, setDescricao] = useState<string>('')
+
   const [id, setId] = useState<string>('')
-  const [cost, setCost] = useState<any[]>([])
+
+  const [cost     , setCost  ] = useState<any[]>([])
   const [allCargos, setCargos] = useState<any[]>([])
-  const [cargo, setCargo] = useState<string>('')
-   const [area           , setArea    ] = useState([])
+  const [cargo    , setCargo ] = useState<string>('')
+
+  //===================================== Filter's States
   const [nomeFilter           ,setNomeFilter     ] = useState<string>('')
   const [descricaoFilter      ,setDescricaoFilter] = useState<string>('')
 
+  //===================================== Loading's States
+  const [loading, setLoading] = useState(true);
+
+
+/* 
+==========================================================================================================
+                                        Modal's Functions
+==========================================================================================================
+*/ 
   function openModalFilter() {
     setIsOpenFilter(true)
   }
@@ -52,10 +69,16 @@ export default function CostCenter() {
     setIsOpenNew(false)
   }
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+/* 
+==========================================================================================================
+                                        Crud's Functions
+==========================================================================================================
+*/ 
+
   async function handleLoadCostCenter() {
     const allCostCenter = await centroCustos.list()
     setCost(allCostCenter)
+    setLoading(false)
   }
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -90,20 +113,33 @@ export default function CostCenter() {
     await handleLoadCostCenter()
   }
 
-  useEffect(() => {
-    handleLoadCostCenter()
-  }, [])
 
+  async function handleDelete(id: string) {
+    await centroCustos.delete(id)
+
+    handleLoadCostCenter()
+  }
+
+
+
+
+
+
+/* 
+==========================================================================================================
+                                        Filters's Functions
+==========================================================================================================
+*/ 
   async function handleFilterArea(){
     let filter = ''
 
     if (nomeFilter){
-      console.log("tem nome")
+      
       if(filter.length != 0 ) filter += '&'
       filter += `filter%5Bnome%5D=${nomeFilter}`
     }
     if (descricaoFilter){
-      console.log("tem desc")
+      
 
       if(filter.length != 0 ) filter += '&'
       filter += `filter%5Bdescricao%5D=${descricaoFilter}`
@@ -113,21 +149,31 @@ export default function CostCenter() {
 
     let areaFilted = await centroCustos.listWithManyFilters(filter)
 
-    setArea(areaFilted)
+    setCost(areaFilted)
 
     closeModalFilter()
   }
 
-  async function handleDelete(id: string) {
-    await centroCustos.delete(id)
 
+
+
+/* 
+==========================================================================================================
+                                          UseEffect
+==========================================================================================================
+*/ 
+
+  useEffect(() => {
     handleLoadCostCenter()
-  }
+  }, [])
+
 
   return (
     <>
       <S.Body>
         <Sidebar />
+        <LoadingLayer loading={loading} />
+
         <S.Title>
           <S.Container>Bem vindo, {fullName} 😁</S.Container>
         </S.Title>
